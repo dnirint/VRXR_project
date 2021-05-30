b6705f237b155402d0bf253d087aefa99d83784f
David <david.nir@intel.com>
2021-05-30 20:41:18 +0300

    sadf

---
+++ VR_testing/Assets/Scripts/Projectiles/SelectionStrategy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SelectionStrategy : MonoBehaviour
+{
+
+    public void OnActivateWithTrigger()
+    {
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<XRSimpleInteractable>().onActivate.AddListener((val) => { OnActivateWithTrigger(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VR_testing/Assets/Scripts/Projectiles/XRActivateionScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class XRActivateionScript : MonoBehaviour
+{
+    public void OnActivateWithTrigger()
+    {
+        gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        //gameObject.GetComponent<XRSimpleInteractable>().onActivate.AddListener((val) => { OnActivateWithTrigger(); });
+    }
+}
